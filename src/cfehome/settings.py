dcb71054c151814d35c6681289e6098aaec7be7a
CodingForEntrepreneurs <hello@teamcfe.com>
2025-07-17 17:48:58 -0700

    Share to LinkedIn with Python

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -153,6 +153,7 @@ SOCIALACCOUNT_STORE_TOKENS = True
 #                 "client_id": "<insert-id>",
 #                 "secret": "<insert-secret>",
 #                 "settings": {
+#                     "scope": ["openid", "profile", "w_member_social", "email"],
 #                     "server_url": "https://www.linkedin.com/oauth",
 #                 },
 #             }
