427e8b92f91867ed4ce8eb3984c29ec040208ff8
CodingForEntrepreneurs <hello@teamcfe.com>
2025-07-18 09:49:14 -0700

    18 - Human Readable Model Error Messages

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils import timezone
 
@@ -12,6 +13,18 @@ class Post(models.Model):
     shared_at_linkedin = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def clean(self, *args, **kwargs):
+        super().clean(*args, **kwargs)
+        if len(self.content) < 5:
+            raise ValidationError({
+                "content": "Content must be at least 5 characters long."
+            })
+        elif self.share_on_linkedin and not self.can_share_on_linkedin:
+            raise ValidationError({
+                "share_on_linkedin": f"Content is already shared on LinkedIn at {self.shared_at_linkedin}.",
+                "content": "Content is already shared on LinkedIn."
+            })
 
     def save(self, *args, **kwargs):
         # pre-save
