75502200d48f638aba8aaa870107d0355a618c00
CodingForEntrepreneurs <hello@teamcfe.com>
2025-07-18 09:36:30 -0700

    17 - Using the Django Model Save Method

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 User = settings.AUTH_USER_MODEL # "auth.User"
 
@@ -11,3 +12,18 @@ class Post(models.Model):
     shared_at_linkedin = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        # pre-save
+        if self.share_on_linkedin and self.can_share_on_linkedin:
+            print("sharing on linkedin")
+            self.shared_at_linkedin = timezone.now()
+        else:
+            print('not sharing')
+        self.share_on_linkedin = False
+        super().save(*args, **kwargs)
+        # post-save
+
+    @property
+    def can_share_on_linkedin(self):
+        return not self.shared_at_linkedin
