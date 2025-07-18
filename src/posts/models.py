e47aca7c32622640ff6ac813c537f5a1b681e0ac
CodingForEntrepreneurs <hello@teamcfe.com>
2025-07-18 16:31:13 -0700

    29 - Prevent Duplicate Share Events

--- src/posts/models.py
+++ src/posts/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     content = models.TextField()
     share_now = models.BooleanField(default=None, null=True, blank=True)
     share_at = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
-
+    share_complete_at = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
     share_on_linkedin = models.BooleanField(default=False)
     shared_at_linkedin = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -43,7 +43,10 @@ class Post(models.Model):
     def save(self, *args, **kwargs):
         # pre-save
         do_schedule_post = False
-        if self.share_now is not None or self.share_at is not None:
+        if all([
+            self.share_now is not None or self.share_at is not None,
+            self.share_complete_at is None
+        ]):
             do_schedule_post = True
         super().save(*args, **kwargs)
 
@@ -51,6 +54,7 @@ class Post(models.Model):
             inngest_client.send_sync(
                 inngest.Event(
                     name="posts/post.scheduled",
+                    id=f"posts/post.scheduled.{self.id}",
                     data={"object_id": self.id}
                 )
             )
