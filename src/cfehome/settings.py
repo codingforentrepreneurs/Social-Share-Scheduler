56ca953b880ed0d87a1df9e14ed940d4cce8aa9b
CodingForEntrepreneurs <hello@teamcfe.com>
2025-07-21 12:42:35 -0700

    37 - Inngest Cloud

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,7 +32,9 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 if DEBUG:
-    ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = ['*', '.hungrypy.com']
+
+CSRF_TRUSTED_ORIGINS = ['https://*.hungrypy.com']
 
 # Application definition
 
